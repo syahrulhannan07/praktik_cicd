60e2ce9ff62e28c052e7b59c0f88c77b869fb545
syahrulhannan07 <syahrulhannan07@gmail.com>
2026-02-26 20:59:00 +0700

    push automasi cicd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CICD AUTOMASI LARAVEL</title>
+        <title>CICD AUTOMASI</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
